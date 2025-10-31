d82dd6b70f277f364ea45034b9c672e93d41b820
Dmytryi <muzalevskyid333@gmail.com>
2025-10-31 10:42:22 +0200

    Initial commit

---
+++ Wow.Core/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wow.Core.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+        public User(string name, string email, string password) { 
+        this.UserId = Guid.NewGuid();
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+        
+        }
+
+
+
+
+    }
+}
---
+++ Wow/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        bool running = true;
+
+        while (running) {
+
+            Console.WriteLine("===================================================");
+            Console.WriteLine("1. Start game");
+            Console.WriteLine("2. Load game");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine();
+
+
+            switch (Console.ReadLine()) {
+                case "1":
+                    Console.WriteLine("Choose your character");
+                    break;
+                case "3":
+                    running = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Невірна команда. Спробуйте ще раз.");
+                    break;
+
+            }
+
+
+        }
+    }
+}
